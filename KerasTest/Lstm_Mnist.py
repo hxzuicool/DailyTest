d54cd22ae06c181613e7a915a26bbc058e45f77f
hxzuicool <hxzuicool@163.com>
2021-03-23 21:08:57 +0800

    LSTM++

--- KerasTest/Lstm_Mnist.py
+++ KerasTest/Lstm_Mnist.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torchvision import transforms
+import time
 
 
 class Rnn(nn.Module):
@@ -18,7 +19,7 @@ class Rnn(nn.Module):
         out, (h_n, c_n) = self.lstm(x)
         # 此时可以从out中获得最终输出的状态h
         # x = out[:, -1, :]
-        x = h_n[-1, :, :]
+        x = h_n[-1, :, :]  # -1是取最后一层lstm的输出，out和h_n是一样的。
         x = self.classifier(x)
         return x
 
@@ -28,14 +29,13 @@ transform = transforms.Compose([
     transforms.Normalize([0.5], [0.5]),
 ])
 
-trainset = torchvision.datasets.MNIST(root='../data', train=True, download=False, transform=transform)
+trainset = torchvision.datasets.MNIST(root='../data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
 
-testset = torchvision.datasets.MNIST(root='../data', train=False, download=False, transform=transform)
+testset = torchvision.datasets.MNIST(root='../data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
 
-print(len(trainloader))
-net = Rnn(28, 10, 2, 10)
+net = Rnn(in_dim=28, hidden_dim=10, n_layer=2, n_classes=10)  # n_layer=2 为两层lstm堆叠，计算量增大。默认为1
 
 net = net.to('cpu')
 criterion = nn.CrossEntropyLoss()
@@ -52,7 +52,10 @@ def train(epoch):
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to('cpu'), targets.to('cpu')
         optimizer.zero_grad()
-        outputs = net(torch.squeeze(inputs, 1))
+        outputs = net(torch.squeeze(inputs, 1))  # net(128, 28, 28)  输入是128批量的28*28图片. 可以有c_0和h_0，因为是第一个，所以也可以没有.
+        # net(torch.squeeze(inputs, 1), (c_0, h_0))
+        #  h_n是上一层输出的结果，c_n是上一层调整后的记忆
+        # outputs.shape = 128, 10   输出是128批量的 10个预测值
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
@@ -67,6 +70,7 @@ def train(epoch):
 
 
 def test(epoch):
+    print('\nEpoch: %d' % epoch)
     global best_acc
     net.eval()
     test_loss = 0
@@ -87,6 +91,12 @@ def test(epoch):
                   % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
 
 
-for epoch in range(200):
-    train(epoch)
-    test(epoch)
+if __name__ == '__main__':
+    start = time.time()
+    # net = torch.load('../model/lstm_minst.pt')
+    for epoch in range(2):
+        train(epoch)
+        test(epoch)
+
+    # torch.save(net, '../model/lstm_minst.pt')
+    print(time.time() - start)
---
+++ Paddle/17.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import transforms
+
+rnn = nn.LSTM(4, 10, 2)  # input_size, hidden_size, num_layers
+input = torch.rand(5, 3, 4)  # sqe_len, batch_size, input_size
+# print(input)
+h0 = torch.rand(2, 3, 10)
+print(h0)
+c0 = torch.rand(2, 3, 10)
+output, (hn, cn) = rnn(input, (h0, c0))
+
+# print(output)
